fbbedf7046ec6e97f0c9006795556c1d537e57bc
csf30816 <csf30816@users.noreply.github.com>
2017-09-09 19:48:30 -1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 var scratcher = "csf30816";
 
 var http = {
-	get: function(e,t){var n=new XMLHttpRequest;n.onreadystatechange=function(){4==n.readyState&&200==n.status&&t(n.responseText)},n.open("GET",e,!0),n.send(null)},
+	get: function(e){var t=new XMLHttpRequest;return t.open("GET",e,!1),t.send(null),t.responseText},
 	post: function(e,t,n){var o="string"==typeof t?t:Object.keys(t).map(function(e){return encodeURIComponent(e)+"="+encodeURIComponent(t[e])}).join("&"),s=window.XMLHttpRequest?new XMLHttpRequest:new ActiveXObject("Microsoft.XMLHTTP");return s.open("POST",e),s.onreadystatechange=function(){s.readyState>3&&200==s.status&&n(s.responseText)},s.setRequestHeader("X-Requested-With","XMLHttpRequest"),s.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),s.send(o),s}
 };
 
 var info = {
-	messages: http.get('https://api.scratch.mit.edu/users/csf30816/messages/count', function(data){return data;}),
+	messages: http.get('https://api.scratch.mit.edu/users/csf30816/messages/count'),
 };
