30201a168bece7652b35edf20b04a13842e1169c
csf30816 <csf30816@users.noreply.github.com>
2017-09-10 14:59:55 -1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,9 @@ var scratcher = "csf30816";
 
 var http = {
 	// HTTP GET Request function
-	get: function(e) {
-		var t = new XMLHttpRequest;
-		return t.open("GET", e, !1), t.send(null), t.responseText
-	},
+	get: function(e){var t=new XMLHttpRequest;return t.open("GET",e,!1),t.send(null),t.responseText},
 	// HTML POST Request function WIP
-	post: function(e, t, n) {
-		var o = "string" == typeof t ? t : Object.keys(t).map(function(e) {
-				return encodeURIComponent(e) + "=" + encodeURIComponent(t[e])
-			}).join("&"),
-			s = window.XMLHttpRequest ? new XMLHttpRequest : new ActiveXObject("Microsoft.XMLHTTP");
-		return s.open("POST", e), s.onreadystatechange = function() {
-			s.readyState > 3 && 200 == s.status && n(s.responseText)
-		}, s.setRequestHeader("X-Requested-With", "XMLHttpRequest"), s.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"), s.send(o), s
-	}
+	post: function(e,t,n){var o="string"==typeof t?t:Object.keys(t).map(function(e){return encodeURIComponent(e)+"="+encodeURIComponent(t[e])}).join("&"),s=window.XMLHttpRequest?new XMLHttpRequest:new ActiveXObject("Microsoft.XMLHTTP");return s.open("POST",e),s.onreadystatechange=function(){s.readyState>3&&200==s.status&&n(s.responseText)},s.setRequestHeader("X-Requested-With","XMLHttpRequest"),s.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),s.send(o),s}
 };
 
 // Scratcher's basic info
